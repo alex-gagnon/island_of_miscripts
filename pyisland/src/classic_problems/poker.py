37c0f1136cea25e44af18d72e63feff575b1dc53
alex-gagnon <28638888+alex-gagnon@users.noreply.github.com>
2020-06-07 23:58:56 -0400

    Updated poker for readability and updated tests

--- pyisland/src/classic_problems/poker.py
+++ pyisland/src/classic_problems/poker.py
@@ -1,5 +1,6 @@
+import itertools
+import sys
 from collections import defaultdict, namedtuple
-from random import choice
 
 Card = namedtuple('Card', ['rank', 'suit'])
 
@@ -9,131 +10,78 @@ class Hand:
 
 
 class Poker:
-    """ Find a winning Poker hand. """
+    """ Find a winning Poker hand. 
+    Score Card:
+        8: Royal Flush* - Ten, Jack, Queen, King, Ace of the same suit
+        8: Straight Flush* - Consecutive values of the same suit
+        7: Four of a Kind - Four cards of the same value
+        6: Full House - Three of a kind and a pair
+        5: Flush - All cards of the same suit
+        4: Straight - All cards are consecutive values
+        3: Three of a Kind - Three cards of the same value
+        2: Two Pair - Two different pairs
+        1: One Pair - Two cards of the same value
+        0: High Card - Highest value card
+
+        * Royal and straight flush are evaluated together as highest value wins.
+    """
 
     ranks = [str(n) for n in range(2, 10)] + list("TJQKA")
-    rank_values = {rank: i + 2 for i, rank in enumerate(ranks)}
+    rank_values = {rank: i for i, rank in enumerate(ranks, 2)}
 
     def __init__(self, hand):
         self.hand = Hand(hand).hand
-        self.hand_ranks = self.rank_hand()
+        self.hand_rank_values = sorted([self.rank_values.get(card.rank) for card in self.hand], reverse=True)
+        self.hand_suits = [card.suit for card in self.hand]
 
     def __str__(self):
         return str(self.hand)
     
-    def rank_hand(self):
-        ranks = ['..23456789TJQKA'.index(rank) for rank, suit in self.hand]
-        ranks.sort(reverse=True)
-        # Handle Ace rank as lowest value card
-        return [5, 4, 3, 2, 1] if ranks == [14, 5, 4, 3, 2] else ranks
+    def eval_hand(self):
+        # Evaluate for Royal and Straight Flush
+        if self.straight_flush(): 
+            return 8, self.hand_rank_values[0]
+        # Evaluate for Flush
+        if self.flush(): 
+            return 5, self.hand_rank_values
+        # Evaluate for Straight
+        if self.straight(): 
+            return 4, self.hand_rank_values[1]
 
-    def count_values(self):
-        values = [card.rank for card in self.hand]
-        value_counts = defaultdict(lambda: 0)
-        for v in values:
-            value_counts[v] += 1
-        return value_counts
-
-    def find_best_hand(self):
-        """ Returns the best possible win with current hand. """
-        winning_hands = {
-            0: {
-                "name": "High Card",
-                "function": self.high_card
-            },
-            1: {
-                "name": "One Pair",
-                "function": self.one_pair
-            },
-            2: {
-                "name": "Two Pairs",
-                "function": self.two_pairs
-            },
-            3: {
-                "name": "Three of a Kind",
-                "function": self.three_of_a_kind
-            },
-            4: {
-                "name": "Straight",
-                "function": self.straight
-            },
-            5: {
-                "name": "Flush",
-                "function": self.flush
-            },
-            6: {
-                "name": "Full House",
-                "function": self.full_house
-            },
-            7: {
-                "name": "Four of a Kind",
-                "function": self.four_of_a_kind
-            },
-            8: {
-                "name": "Straight Flush",
-                "function": self.straight_flush
-            },
-            9: {
-                "name": "Royal Flush",
-                "function": self.royal_flush
-            }
-        }
-        for k, v in reversed(winning_hands.items()):
-            if v['function']():
-                return k, v['name'], self.hand_ranks
-
-    def royal_flush(self):
-        ranks = [card for card in self.hand if card.rank not in [str(n) for n in range(2, 10)]]
-        return len(ranks) == 5 and self.straight_flush()
+        trips = []
+        pairs = []
+        for v, group in itertools.groupby(self.hand_rank_values):
+            # Count for identical hand rank values
+            count = sum(1 for _ in group)
+            # Evaluate for Four of a Kind
+            if count == 4: 
+                return 7, v, self.hand_rank_values
+            elif count == 3: 
+                trips.append(v)
+            # Evaluate for Pairs
+            elif count == 2: 
+                pairs.append(v)
+        
+        # Evaluate for Full House Three of a Kind
+        if trips: 
+            return (6 if pairs else 3), trips, pairs, self.hand_rank_values
+        # Returns otherwise for Pairs and High Cards
+        return len(pairs), pairs, self.hand_rank_values
 
     def straight_flush(self):
-        return self.flush() and self.straight()
-
-    def four_of_a_kind(self):
-        values = self.count_values()
-        return sorted(values.values()) == [1, 4]
-
-    def full_house(self):
-        values = self.count_values()
-        return sorted(values.values()) == [2, 3]
+        """ Evaluates for Royal Flush and Straight Flush. """
+        return self.straight() and self.flush()
 
     def flush(self):
-        suits = [card.suit for card in self.hand]
-        return len(set(suits)) == 1
+        # Reduces card suits into a set
+        return len({card.suit for card in self.hand}) == 1
 
     def straight(self):
-        return (max(self.hand_ranks) - min(self.hand_ranks) == 4) and len(set(self.hand_ranks)) == 5
-        # values = self.count_values()
-        # rank_values = self.hand_ranks #[self.hand_ranks[i] for i in values]
-        # value_range = max(rank_values) - min(rank_values)
-        # if len(set(values.values())) == 1 and value_range == 4:
-        #     return True
-        # else:
-        #     if set(values) == set("A 2 3 4 5".split()):
-        #         return True
-        #     else:
-        #         return False
-
-    def three_of_a_kind(self):
-        values = self.count_values()
-        return set(values.values()) == set([3, 1])
-
-    def two_pairs(self):
-        values = self.count_values()
-        if sorted(values.values()) == [1, 2, 2]:
-            self.hand_ranks = self.rank_values.get(max(values, key=values.get))
-            return True
-
-    def one_pair(self):
-        values = self.count_values()
-        if 2 in values.values():
-            self.hand_ranks = self.rank_values.get(max(values, key=values.get))
-            return True
+        high_card, low_card = max(self.hand_rank_values), min(self.hand_rank_values)
+        unique_cards = len(set(self.hand_rank_values))
+        # Difference between lowest and highest card in a straight is always 4, unless it's a low-Ace straight
+        return (high_card - low_card == 4 and unique_cards == 5) or self.hand_rank_values == [14, 5, 4, 3, 2]
     
-    def high_card(self):
-        values = self.count_values()
-        self.hand_ranks = self.rank_values.get(max(values, key=values.get))
-        return True
 
 
 def create_player_hands(hands):
@@ -146,13 +94,18 @@ def create_player_hands(hands):
     player_2 = Poker(hands.split()[5:])
     return player_1, player_2
 
-
 def play(hands):
+    """ Returns winner from Poker game. """
     player_1, player_2 = create_player_hands(hands)
-    players = {
-        "Player 1": player_1.find_best_hand(),
-        "Player 2": player_2.find_best_hand()
-        }
-    print(players)
-    winner = max(players, key=players.get)
+    winner = "Player 1" if player_1.eval_hand() > player_2.eval_hand() else "Player 2"
     return winner
+
+# Hacker Rank
+def run_hacker_rank():
+    def get_hands():
+        lines = [line.rstrip() for line in sys.stdin.readlines()]
+        cases = lines.pop(0)
+        return [play(hands) for hands in lines]
+    results = get_hands()
+    for winner in results:
+        print(winner)
